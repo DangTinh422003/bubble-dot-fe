b58cf7c7421120ddc5d49979925f521451cc5aff
Dang Tinh <caodangtinh04022003@gmail.com>
2024-10-22 21:05:02 +0700

    update: download and build

--- app/components/workbench/Workbench.client.tsx
+++ app/components/workbench/Workbench.client.tsx
@@ -1,13 +1,13 @@
+import { AiOutlineLoading3Quarters } from 'react-icons/ai';
 import { useStore } from '@nanostores/react';
 import { motion, type HTMLMotionProps, type Variants } from 'framer-motion';
 import { computed } from 'nanostores';
-import { memo, useCallback, useEffect } from 'react';
+import { memo, useCallback, useEffect, useState } from 'react';
 import { toast } from 'react-toastify';
 import {
   type OnChangeCallback as OnEditorChange,
   type OnScrollCallback as OnEditorScroll,
 } from '~/components/editor/codemirror/CodeMirrorEditor';
-import { IconButton } from '~/components/ui/IconButton';
 import { PanelHeaderButton } from '~/components/ui/PanelHeaderButton';
 import { Slider, type SliderOptions } from '~/components/ui/Slider';
 import { workbenchStore, type WorkbenchViewType } from '~/lib/stores/workbench';
@@ -53,10 +53,11 @@ const workbenchVariants = {
 } satisfies Variants;
 
 const BASE_URL = 'http://localhost:3000';
-// const BASE_URL = 'http://13.250.113.64:3000';
 
 export const Workbench = memo(({ chatStarted, isStreaming }: WorkspaceProps) => {
   renderLogger.trace('Workbench');
+
+  const [isLoading, setIsLoading] = useState(false);
 
   const hasPreview = useStore(computed(workbenchStore.previews, (previews) => previews.length > 0));
   const showWorkbench = useStore(workbenchStore.showWorkbench);
@@ -70,37 +71,51 @@ export const Workbench = memo(({ chatStarted, isStreaming }: WorkspaceProps) => 
     workbenchStore.currentView.set(view);
   };
 
-  useEffect(() => {
-    (async () => {
-      if (!isStreaming && files) {
-        try {
-          console.log('Generate completed!');
+  const downloadFiles = (fileData: { [key: string]: string }) => {
+    if (fileData) {
+      Object.entries(fileData).forEach(([fileName, fileContent]) => {
+        const blob = new Blob([fileContent], { type: 'text/plain' });
+        const link = document.createElement('a');
+        link.href = URL.createObjectURL(blob);
+        link.download = fileName;
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+      });
+    }
+  };
 
-          const downdloadBucketRes = await fetch(`${BASE_URL}/cloud/download-bucket`, {
+  const handleBuild = async () => {
+    if (!isStreaming && files) {
+      try {
+        console.log('Generate completed!');
+        setIsLoading(true);
+
+        const downdloadBucketRes = await fetch(`${BASE_URL}/cloud/download-bucket`, {
+          method: 'POST',
+        });
+
+        const downdloadBucketData: { status: string } = await downdloadBucketRes.json();
+
+        if (downdloadBucketData.status === 'success') {
+          const buildRes = await fetch(`${BASE_URL}/cloud/build`, {
             method: 'POST',
           });
 
-          const downdloadBucketData: { status: string } = await downdloadBucketRes.json();
+          const buildData: {
+            data: {
+              [key: string]: string;
+            };
+          } = await buildRes.json();
 
-          if (downdloadBucketData.status === 'success') {
-            const buildRes = await fetch(`${BASE_URL}/cloud/build`, {
-              method: 'POST',
-            });
-
-            const buildData: {
-              data: {
-                [key: string]: string;
-              };
-            } = await buildRes.json();
-
-            console.log(buildData.data);
+          if (buildData.data) {
+            setIsLoading(false);
+            downloadFiles(buildData.data);
           }
-        } catch (error) {
-          console.log('🚀 ~ error:', error);
         }
-      }
-    })();
-  }, [isStreaming, files]);
+      } catch (error) {}
+    }
+  };
 
   useEffect(() => {
     if (hasPreview) {
@@ -156,6 +171,26 @@ export const Workbench = memo(({ chatStarted, isStreaming }: WorkspaceProps) => 
               <div className="flex items-center px-3 py-2 pt-4 border-b border-bolt-elements-borderColor bg-[#21222c]">
                 <Slider selected={selectedView} options={sliderOptions} setSelected={setSelectedView} />
                 <div className="ml-auto" />
+                {!isStreaming && files && (
+                  <button
+                    disabled={isLoading}
+                    className="flex items-center shrink-0 gap-1.5 px-1.5 rounded-md py-0.5 text-bolt-elements-item-contentDefault bg-transparent enabled:hover:text-bolt-elements-item-contentActive enabled:hover:bg-bolt-elements-item-backgroundActive disabled:cursor-not-allowed mr-10"
+                    onClick={handleBuild}
+                  >
+                    <span>Build and Download</span>
+                    {isLoading && <AiOutlineLoading3Quarters className="animate-spin text-sm" />}
+                  </button>
+                )}
+                {!isStreaming && files && (
+                  <a
+                    target="_blank"
+                    href="https://ui.use.ink/"
+                    className="flex items-center hover:opacity-80 shrink-0 gap-1.5 px-1.5 rounded-md py-0.5 text-bolt-elements-item-contentDefault bg-transparent enabled:hover:text-bolt-elements-item-contentActive enabled:hover:bg-bolt-elements-item-backgroundActive disabled:cursor-not-allowed mr-10"
+                    onClick={handleBuild}
+                  >
+                    <span>Test Contract</span>
+                  </a>
+                )}
                 {selectedView === 'code' && (
                   <PanelHeaderButton
                     className="mr-1 text-sm"
@@ -167,14 +202,6 @@ export const Workbench = memo(({ chatStarted, isStreaming }: WorkspaceProps) => 
                     Toggle Terminal
                   </PanelHeaderButton>
                 )}
-                <IconButton
-                  icon="i-ph:x-circle"
-                  className="-mr-1"
-                  size="xl"
-                  onClick={() => {
-                    workbenchStore.showWorkbench.set(false);
-                  }}
-                />
               </div>
               <div className="relative flex-1 overflow-hidden">
                 <View
