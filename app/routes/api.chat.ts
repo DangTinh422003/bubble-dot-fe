c49cc2067cdc90a6912c9435db91c8d6fa0cc805
Dang Tinh <caodangtinh04022003@gmail.com>
2024-10-25 16:30:58 +0700

    fix: build

--- app/routes/api.chat.ts
+++ app/routes/api.chat.ts
@@ -1,5 +1,5 @@
 import { type ActionFunctionArgs } from '@remix-run/cloudflare';
-import { MAX_RESPONSE_SEGMENTS, MAX_TOKENS } from '~/lib/.server/llm/constants';
+import { MAX_RESPONSE_SEGMENTS } from '~/lib/.server/llm/constants';
 import { CONTINUE_PROMPT } from '~/lib/.server/llm/prompts';
 import { streamText, type Messages, type StreamingOptions } from '~/lib/.server/llm/stream-text';
 import SwitchableStream from '~/lib/.server/llm/switchable-stream';
